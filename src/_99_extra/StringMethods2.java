2fed77907424feac996e8753823e332e7fcf7cd1
root <root@f5fe811094bc>
2020-10-03 20:44:04 +0000

    Week 9?

--- src/_99_extra/StringMethods2.java
+++ src/_99_extra/StringMethods2.java
@@ -3,10 +3,29 @@ package _99_extra;
 
 public class StringMethods2 {
 	public static void main(String[] args) {
-		
+		String random = "TAfonElkl";
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
-
+		System.out.println(random.toUpperCase()); 
+		System.out.println(random.toLowerCase());
+		System.out.println(random.charAt(0) + "" + random.charAt(1) + "" + random.charAt(2));
+		int i = random.length();
+		if(i % 2 == 0) {
+			i = i/2;
+			System.out.println(random.charAt(i));
+		}
+		else {
+		
+			i = i/2;
+			System.out.println(random.charAt(i));
+		}
+		int r = random.length();
+System.out.println(random.charAt(r-3) + "" + random.charAt(r-2) + "" + random.charAt(r-3));
+		
+		
+		
+		//System.out.println("half of six is " + 6/2);
+		//System.out.println("half of seven is " + 7/2 + " value 3.5 gets truncates to 3, meaning removal of decimals");
 		// 2. Print your String to the console in upper case.
 		
 		// 3. Print your String to the console in lower case.
